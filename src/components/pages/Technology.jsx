75c41e514d577726fcb21976088f19d85063e6a7
kyoko <kevin.kbr123@gmail.com>
2022-04-22 18:40:39 -0400

    Separando en componentes la pestaña "Technology".

--- src/components/pages/Technology.jsx
+++ src/components/pages/Technology.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Box, Flex, Image, Spinner, Text } from "@chakra-ui/react";
+import { Box, Spinner, Text } from "@chakra-ui/react";
 
 import { Header } from "../sections/Header";
 import { TitleItem } from "../items/Title_item";
-import { ButtonTechnologyItem } from "../items/Button_Technology_item";
+import { TechnologyItem } from "../items/Technology_item";
 import { fetchApi } from "../../services/fetchAPI";
 
 import bgTechnology from "/assets/technology/background-technology-desktop.jpg";
@@ -36,96 +36,13 @@ export const Technology = () => {
 					Cargando... <Spinner as="span" color="white.500" size="lg" />
 				</Text>
 			) : (
-				<Flex
-					flexDir={{
-						lg: "row",
-						md: "column-reverse",
-						base: "column-reverse"
-					}}
-					w="100vw"
-					h={{ lg: "24rem", md: "80vh", base: "39rem" }}
-					m="0 auto"
-					justifyContent={{
-						lg: "space-around",
-						md: "space-around",
-						base: "space-evenly"
-					}}
-				>
-					<Box
-						display="flex"
-						justifyContent={{
-							lg: "space-evenly",
-							md: "space-evenly",
-							base: "space-around"
-						}}
-						flexDir={{ lg: "row", md: "column", base: "column" }}
-						h={{ lg: "auto", md: "50%", base: "55%" }}
-					>
-						<Flex
-							display="flex"
-							flexDir={{ lg: "column", md: "row", base: "row" }}
-							gap={7}
-							justifyContent="center"
-						>
-							{Array(3)
-								.fill(null)
-								.map((_, i) => (
-									<ButtonTechnologyItem
-										key={i}
-										number={i + 1}
-										slideTechnology={() => setTechnologyValues(i)}
-										index={i}
-									/>
-								))}
-						</Flex>
-
-						<Box
-							display="flex"
-							flexDir="column"
-							justifyContent={{
-								lg: "center",
-								md: "space-evenly",
-								base: "space-evenly"
-							}}
-							h={{ lg: "auto", md: "60%" }}
-							m={{ lg: "0", md: "0 auto", base: "0 auto" }}
-							textAlign={{ lg: "initial", md: "center", base: "center" }}
-							w={{ lg: "50%", md: "80%", base: "90%" }}
-						>
-							<Text
-								letterSpacing="4.75px"
-								fontFamily="Barlow Condensed"
-								color="#D0D6F9"
-							>
-								THE TERMINOLOGY...
-							</Text>
-							<Text fontSize={{ lg: "5xl", md: "5xl", base: "4xl" }}>
-								{technology[technologyValues].name.toUpperCase()}
-							</Text>
-							<Text fontFamily="Barlow" color="#D0D6F9">
-								{technology[technologyValues].description}
-							</Text>
-						</Box>
-					</Box>
-					{/* Picture se puede utilizar como MediaQuery */}
-					<Box
-						as="picture"
-						h={{ lg: "inherit", md: "30%", base: "20%" }}
-						w={{ lg: "70%", md: "inherit", base: "inherit" }}
-					>
-						<source
-							srcSet={technology[technologyValues].images.landscape}
-							media="(max-width: 990px)"
-						/>
-						<Image
-							src={technology[technologyValues].images.portrait}
-							mt={{ lg: "-4rem", md: "0", base: "0" }}
-							alt="techcology"
-							objectFit="contain"
-							m="0 auto"
-						/>
-					</Box>
-				</Flex>
+				<TechnologyItem
+					name={technology[technologyValues].name}
+					description={technology[technologyValues].description}
+					imgLandscape={technology[technologyValues].images.landscape}
+					imgPortrait={technology[technologyValues].images.portrait}
+					setTechnologyValues={setTechnologyValues}
+				/>
 			)}
 		</Box>
 	);
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/space_tourism/',
+  // *Activar para hacer Deploy en Github Pages
+  // base:'/space_tourism/',
   plugins: [react()]
 })
